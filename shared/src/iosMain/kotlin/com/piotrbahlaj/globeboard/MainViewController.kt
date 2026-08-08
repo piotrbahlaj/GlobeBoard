c9d63f55552c10b72e13d021204e7980d0e913fe
Piotr Bahłaj <piotr.bahlaj@o2.pl>
2026-08-08 05:01:39 +0200

    Initialize koin

--- shared/src/iosMain/kotlin/com/piotrbahlaj/globeboard/MainViewController.kt
+++ shared/src/iosMain/kotlin/com/piotrbahlaj/globeboard/MainViewController.kt
@@ -1,5 +1,12 @@
 package com.piotrbahlaj.globeboard
 
 import androidx.compose.ui.window.ComposeUIViewController
+import com.piotrbahlaj.globeboard.core.di.initKoin
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController {
+    App()
+}
+
+fun doInitKoin() {
+    initKoin()
+}
